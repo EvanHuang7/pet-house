8e3d5b233f95d566e971060c63c65ad9db32863e
Zhihui Huang <zhh399@mail.usask.ca>
2025-09-06 21:00:38 -0600

    add getter and setter methods for Pet table

--- pet-service/src/main/java/com/ph/petservice/model/Pet.java
+++ pet-service/src/main/java/com/ph/petservice/model/Pet.java
@@ -36,4 +36,51 @@ public class Pet  {
 //    @JoinColumn(name = "owner_id", nullable = false)
 //    private User owner;
 
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public LocalDate getDate0fBirth() {
+        return date0fBirth;
+    }
+
+    public void setDate0fBirth(LocalDate date0fBirth) {
+        this.date0fBirth = date0fBirth;
+    }
+
+    public LocalDate getRegisteredDate() {
+        return registeredDate;
+    }
+
+    public void setRegisteredDate(LocalDate registeredDate) {
+        this.registeredDate = registeredDate;
+    }
 }
