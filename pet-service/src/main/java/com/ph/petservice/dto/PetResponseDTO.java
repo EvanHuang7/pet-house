a6b1137ad9e656822dea65ced850571e722a368f
Zhihui Huang <zhh399@mail.usask.ca>
2025-09-08 18:32:52 -0600

    add PetResponseDTO class for the latter PetService to use

---
+++ pet-service/src/main/java/com/ph/petservice/dto/PetResponseDTO.java
@@ -0,0 +1,49 @@
+package com.ph.petservice.dto;
+
+public class PetResponseDTO {
+    private String id;
+    private String name;
+    private String type;
+    private String breed;
+    private String dateOfBirth;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
