e86cbba4a6b1358570b1a963f2c9154b39133eab
Zhihui Huang <zhh399@mail.usask.ca>
2025-09-08 20:53:58 -0600

    init pet controller

---
+++ pet-service/src/main/java/com/ph/petservice/controller/PetController.java
@@ -0,0 +1,15 @@
+package com.ph.petservice.controller;
+
+import com.ph.petservice.service.PetService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pets")
+public class PetController {
+    private final PetService petService;
+
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
+}
