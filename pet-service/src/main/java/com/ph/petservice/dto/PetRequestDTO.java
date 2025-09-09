70a4fdc9db0378171468864e665f853ea167184d
Zhihui Huang <zhh399@mail.usask.ca>
2025-09-08 21:18:54 -0600

    Add PetRequestDTO class

---
+++ pet-service/src/main/java/com/ph/petservice/dto/PetRequestDTO.java
@@ -0,0 +1,64 @@
+package com.ph.petservice.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class PetRequestDTO {
+    @NotBlank(message = "Name is required")
+    @Size(max = 100, message = "Name cannot exceed 100 characters")
+    private String name;
+
+    @NotBlank(message = "Type is required")
+    @Size(max = 100, message = "Type cannot exceed 100 characters")
+    private String type;
+
+    @NotBlank(message = "Breed is required")
+    @Size(max = 100, message = "Breed cannot exceed 100 characters")
+    private String breed;
+
+    @NotBlank(message = "Date of birth is required")
+    private String dateOfBirth;
+
+    @NotBlank(message = "Registered date is required")
+    private String registeredDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getRegisteredDate() {
+        return registeredDate;
+    }
+
+    public void setRegisteredDate(String registeredDate) {
+        this.registeredDate = registeredDate;
+    }
+}
