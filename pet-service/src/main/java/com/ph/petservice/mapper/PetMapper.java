31416808a74f24d3dc99c977670772c5e1b80b4d
Zhihui Huang <zhh399@mail.usask.ca>
2025-09-08 21:29:51 -0600

    add requestDTOToModel method to PetMapper class

--- pet-service/src/main/java/com/ph/petservice/mapper/PetMapper.java
+++ pet-service/src/main/java/com/ph/petservice/mapper/PetMapper.java
@@ -1,7 +1,10 @@
 package com.ph.petservice.mapper;
 
+import com.ph.petservice.dto.PetRequestDTO;
 import com.ph.petservice.dto.PetResponseDTO;
 import com.ph.petservice.model.Pet;
+
+import java.time.LocalDate;
 
 public class PetMapper {
     public static PetResponseDTO toDTO(Pet pet) {
@@ -14,5 +17,17 @@ public class PetMapper {
         petDTO.setDateOfBirth(pet.getDateOfBirth().toString());
 
         return petDTO;
+    }
+
+    public static Pet requestDTOToModel(PetRequestDTO petDTO) {
+        Pet pet = new Pet();
+
+        pet.setName(petDTO.getName());
+        pet.setType(petDTO.getType());
+        pet.setBreed(petDTO.getBreed());
+        pet.setDateOfBirth(LocalDate.parse(petDTO.getDateOfBirth()));
+        pet.setRegisteredDate(LocalDate.parse(petDTO.getRegisteredDate()));
+
+        return pet;
     }
 }
