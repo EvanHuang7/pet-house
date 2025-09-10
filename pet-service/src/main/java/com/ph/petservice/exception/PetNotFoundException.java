234aba3fa1f95896148fe8acdcf7673c1929f5bb
Zhihui Huang <zhh399@mail.usask.ca>
2025-09-09 20:47:09 -0600

    add PetNotFoundException file

---
+++ pet-service/src/main/java/com/ph/petservice/exception/PetNotFoundException.java
@@ -0,0 +1,7 @@
+package com.ph.petservice.exception;
+
+public class PetNotFoundException extends RuntimeException {
+    public PetNotFoundException(String message) {
+        super(message);
+    }
+}
