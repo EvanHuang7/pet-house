5b7cfafed032c4827fda791bf02bea488111cf20
Zhihui Huang <zhh399@mail.usask.ca>
2025-09-09 21:00:34 -0600

    add updatePet service function

--- pet-service/src/main/java/com/ph/petservice/service/PetService.java
+++ pet-service/src/main/java/com/ph/petservice/service/PetService.java
@@ -2,13 +2,15 @@ package com.ph.petservice.service;
 
 import com.ph.petservice.dto.PetRequestDTO;
 import com.ph.petservice.dto.PetResponseDTO;
+import com.ph.petservice.exception.PetNotFoundException;
 import com.ph.petservice.mapper.PetMapper;
 import com.ph.petservice.model.Pet;
 import com.ph.petservice.repository.PetRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class PetService {
@@ -29,8 +31,25 @@ public class PetService {
     }
 
     public PetResponseDTO createPet(PetRequestDTO petDTO) {
+        // Create a new pet
         Pet createdPet = petRepository.save(PetMapper.requestDTOToModel(petDTO));
 
         return PetMapper.modelToResponseDTO(createdPet);
+    }
+
+    public PetResponseDTO updatePet(UUID id, PetRequestDTO petDTO) {
+        // Get the pet by id
+        Pet targetPet = petRepository.findById(id).orElseThrow(() -> new PetNotFoundException("Pet not found with ID: " + id));
+
+        // Update pet info
+        targetPet.setName(petDTO.getName());
+        targetPet.setType(petDTO.getType());
+        targetPet.setBreed(petDTO.getBreed());
+        targetPet.setDateOfBirth(LocalDate.parse(petDTO.getDateOfBirth()));
+
+        Pet updatedPet = petRepository.save(targetPet);
+
+        return PetMapper.modelToResponseDTO(updatedPet);
+
     }
 }
