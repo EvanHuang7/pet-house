04296ad3124d3924b1513be5f63659c36a288152
Zhihui Huang <zhh399@mail.usask.ca>
2025-09-09 20:52:06 -0600

    add handlePetNotFoundException method in GlobalExceptionHandler class

--- pet-service/src/main/java/com/ph/petservice/exception/GlobalExceptionHandler.java
+++ pet-service/src/main/java/com/ph/petservice/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.ph.petservice.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,11 +13,24 @@ import java.util.Map;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationException(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
 
         ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(PetNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handlePetNotFoundException(PetNotFoundException ex) {
+        log.warn("Pet not found {}", ex.getMessage());
+
+        Map<String, String> errors = new HashMap<>();
+
+        errors.put("message", "Pet not found");
 
         return ResponseEntity.badRequest().body(errors);
     }
